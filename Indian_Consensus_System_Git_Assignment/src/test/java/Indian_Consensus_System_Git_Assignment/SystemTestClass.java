05e88fe43fd93f92b8599b4e36cd53ce55dd83d8
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-10-15 19:26:23 +0530

    Added TC1.4 : Successfully created and executed a separate test case function to check TC1.4 functionality

--- Indian_Consensus_System_Git_Assignment/src/test/java/Indian_Consensus_System_Git_Assignment/SystemTestClass.java
+++ Indian_Consensus_System_Git_Assignment/src/test/java/Indian_Consensus_System_Git_Assignment/SystemTestClass.java
@@ -10,13 +10,11 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.rules.ExpectedException;
 
-import Trying.gradle.project.csv.json.CensusAnalyserException;
-import Trying.gradle.project.csv.json.CensusAnalyzer;
-
 public class SystemTestClass {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "desktop/IndiaStateCensusData.csv"; // no file present in this
 	private static final String INDIA_CENSUS_EMPTY_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusDataEmpty.csv";
+	private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimeter.csv";
 	// path
 	// Default Test
 
@@ -33,8 +31,8 @@ public class SystemTestClass {
 	public void given_Indian_Census_CSV_File_Returns_Correct_Records() {
 		try {
 			CensusAnalyzer censusAnalyser = new CensusAnalyzer();
-			int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
-			Assert.assertEquals(2, numOfRecords);
+			int numberOfEntries = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			Assert.assertEquals(2, numberOfEntries);
 		} catch (CensusAnalyserException e) {
 		}
 	}
@@ -56,16 +54,34 @@ public class SystemTestClass {
 		}
 	}
 
-	// Handles exception when wrong file type/ empty file is given as an input to loadIndiaCensusData
+	// Handles exception when wrong file type/ empty file is given as an input to
+	// loadIndiaCensusData
 	// function
 	// TC1.3
+	@Ignore
 	@Test
 	public void given_Empty_Csv_File_Should_Return_Custom_Exception_Type() {
 		try {
 			CensusAnalyzer censusAnalyser = new CensusAnalyzer();
-			int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_EMPTY_CSV_FILE_PATH);
+			int numberOfEntries = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_EMPTY_CSV_FILE_PATH);
 		} catch (CensusAnalyserException e) {
 			System.out.println("Oops! There's an exception, but it's handled. So, no worries...");
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+		}
+	}
+
+	// Handles exception when file with wrong delimeter is given as an input to
+	// loadIndiaCensusData
+	// function
+	// TC1.4
+	@Test
+	public void given_Wrong_Delimiter_In_India_Census_Data_Should_Return_Custom_Exception_Type() {
+		try {
+			CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+			int numberOfEntries = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_CSV_WRONG_DELIMITER);
+		} catch (CensusAnalyserException e) {
+			System.out.println("Oops! There's an exception, but it's handled. So, no worries...");
+			System.out.println("e.type is : " + e.type);
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
 		}
 	}
